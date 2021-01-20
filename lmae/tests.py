76a102a0e387b67f3bca0cc8a96af640bea7d1db
kirill <kirill.fasmer@gmail.com>
2021-01-20 17:48:26 +0300

    another commit

--- lmae/tests.py
+++ lmae/tests.py
@@ -5,10 +5,10 @@ class BasicTest(SimpleTestCase):
     def setUp(self):
         # Every test needs a client
         self.client = Client()
-        self.var = 2
+        self.var = 5
 
     def test_self(self):
-        self.assertEqual(self.var, 2)
+        self.assertEqual(self.var, 5)
     
     def test_home(self):        
         response = self.client.get('/')
